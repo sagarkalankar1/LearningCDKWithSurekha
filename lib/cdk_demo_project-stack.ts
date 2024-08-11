18e684db091210efb36830d140a8524b623691d2
Sagar Kalankar <incsqwd1@amway.com>
2024-08-11 22:20:06 +0530

    added bucket object extending bucket interface

--- lib/cdk_demo_project-stack.ts
+++ lib/cdk_demo_project-stack.ts
@@ -21,44 +21,85 @@ export class CdkDemoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, targetEnv: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-// Creating array of bucket name and we just need to give names of bucket and using for 
-// loop --> all the buckets will get created
-    let bucketNames: string[] = ["cdk-sag-bucket-01","cdk-sag-bucket-02"];
-
-    for ( var bucketName of bucketNames ){
-      bucketName += targetEnv
-
-      new s3.Bucket(this, bucketName, {
-        bucketName: bucketName,
+    let buckets: CustomBucket[] = [
+      {
+        bucketName: "cdk-sag-bucket-01",
         accessControl: s3.BucketAccessControl.PRIVATE,
         encryption: s3.BucketEncryption.S3_MANAGED,
         versioned: false,
         blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
         removalPolicy: cdk.RemovalPolicy.DESTROY
+      },
+      {
+        bucketName: "cdk-sag-bucket-02",
+        accessControl: s3.BucketAccessControl.PRIVATE,
+        encryption: s3.BucketEncryption.S3_MANAGED,
+        versioned: true,
+        blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+        removalPolicy: cdk.RemovalPolicy.DESTROY
+      }
+    ]
+
+    for ( var bucket of buckets) {
+      let bucketName = bucket.bucketName + targetEnv;
+
+      console.log("Bucket:", bucket);
+
+      new s3.Bucket(this, bucketName, {
+        bucketName: bucketName,
+        accessControl: bucket.accessControl,
+        encryption: bucket.encryption,
+        versioned: bucket.versioned,
+        blockPublicAccess: bucket.blockPublicAccess,
+        removalPolicy: bucket.removalPolicy
       })
-
-      console.log("BucketName:", bucketName);
-
     }
 
-// Earlier, below 2 buckets were created individually
-    new s3.Bucket(this, 'MyFirstBucket1', {
-      bucketName: `cdk-sag-bucket-01-${targetEnv}`,
-      accessControl: s3.BucketAccessControl.PRIVATE,
-      encryption: s3.BucketEncryption.S3_MANAGED,
-      versioned: false,
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-      removalPolicy: cdk.RemovalPolicy.DESTROY
-    })
+// // Creating array of bucket name and we just need to give names of bucket and using for 
+// // loop --> all the buckets will get created
+//     let bucketNames: string[] = ["cdk-sag-bucket-01","cdk-sag-bucket-02"];
 
-    new s3.Bucket(this, 'MyFirstBucket2', {
-      bucketName: `cdk-sag-bucket-02-${targetEnv}`,
-      accessControl: s3.BucketAccessControl.PRIVATE,
-      encryption: s3.BucketEncryption.S3_MANAGED,
-      versioned: false,
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-      removalPolicy: cdk.RemovalPolicy.DESTROY
-    })    
+//     for ( var bucketName of bucketNames ){
+//       bucketName += targetEnv
+
+//       new s3.Bucket(this, bucketName, {
+//         bucketName: bucketName,
+//         accessControl: s3.BucketAccessControl.PRIVATE,
+//         encryption: s3.BucketEncryption.S3_MANAGED,
+//         versioned: false,
+//         blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+//         removalPolicy: cdk.RemovalPolicy.DESTROY
+//       })
+
+//       console.log("BucketName:", bucketName);
+
+//     }
+
+// Earlier, below 2 buckets were created individually
+    // new s3.Bucket(this, 'MyFirstBucket1', {
+    //   bucketName: `cdk-sag-bucket-01-${targetEnv}`,
+    //   accessControl: s3.BucketAccessControl.PRIVATE,
+    //   encryption: s3.BucketEncryption.S3_MANAGED,
+    //   versioned: false,
+    //   blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+    //   removalPolicy: cdk.RemovalPolicy.DESTROY
+    // })
+
+    // new s3.Bucket(this, 'MyFirstBucket2', {
+    //   bucketName: `cdk-sag-bucket-02-${targetEnv}`,
+    //   accessControl: s3.BucketAccessControl.PRIVATE,
+    //   encryption: s3.BucketEncryption.S3_MANAGED,
+    //   versioned: false,
+    //   blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+    //   removalPolicy: cdk.RemovalPolicy.DESTROY
+    // })    
+
+    // new s3.Bucket(this, 'MyFirstBucket', {
+    //   bucketName: `cdk-sag-bucket-1`,
+    //   versioned: true,
+    //   removalPolicy: cdk.RemovalPolicy.DESTROY,
+    //   publicReadAccess: false   
+    // })    
   }
 }
 
